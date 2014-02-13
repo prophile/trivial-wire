9aad0279616a1eff149d7b78b0e5a57a20d50080
Alastair Lynn <arplynn@gmail.com>
2014-02-13 07:58:32 +0000

    Test some very basic properties

--- Tests.hs
+++ Tests.hs
@@ -1,10 +1,26 @@
 module Main(main) where
 
+import Prelude hiding ((.), id)
+
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Function
+
+import Control.FRP.Wire
+import Control.FRP.Wire.Utils
+
+import Control.Category
+import Control.Arrow
+import Control.Applicative
 
 main :: IO ()
 main = hspec $ do
-  describe "the test suite" $ do
-    it "passes this trivial example" $
-      property $ \x -> (x + 1) > (x :: Integer)
+  describe "Wire" $ do
+    it "ignores its input for 'pure'" $ property $
+      \x -> viewWire (pure "bees") [(x :: String)] == ["bees"]
+    it "obeys identity" $ property $
+      \x -> viewWire id x == (x :: String)
+    it "obeys arr" $ property $
+      \x f -> viewWire (arr (apply f)) (x :: [String]) ==
+              (map (apply f) x :: [Int])
+
