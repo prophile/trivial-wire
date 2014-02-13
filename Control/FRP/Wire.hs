9359f4c117c8109292e99548c77e8e39583331a6
Alastair Lynn <arplynn@gmail.com>
2014-02-13 06:58:46 +0000

    Add convenience instances for common typeclasses

--- Control/FRP/Wire.hs
+++ Control/FRP/Wire.hs
@@ -9,13 +9,59 @@ import Control.Arrow
 import Control.Arrow.Operations
 import Control.Arrow.Transformer.Automaton
 
-import Control.Applicative(Applicative)
+import Control.Applicative
+import Data.Monoid
 
 newtype Wire a b = Wire (Automaton (->) a b)
   deriving (Category, Arrow,
             ArrowChoice,
             ArrowLoop, ArrowCircuit,
             Functor, Applicative)
+
+instance (Monoid b) => Monoid (Wire a b) where
+  mempty = pure mempty
+  mappend = liftA2 mappend
+
+instance (Num b) => Num (Wire a b) where
+  (+) = liftA2 (+)
+  (-) = liftA2 (-)
+  (*) = liftA2 (*)
+
+  negate = fmap negate
+
+  signum = fmap signum
+  abs    = fmap abs
+
+  fromInteger = pure . fromInteger
+
+instance (Fractional b) => Fractional (Wire a b) where
+  (/)          = liftA2 (/)
+  recip        = fmap recip
+  fromRational = pure . fromRational
+
+instance (Floating b) => Floating (Wire a b) where
+  pi      = pure pi
+  exp     = fmap exp
+  sqrt    = fmap sqrt
+  log     = fmap log
+  (**)    = liftA2 (**)
+  logBase = liftA2 logBase
+  sin     = fmap sin
+  tan     = fmap tan
+  cos     = fmap cos
+  asin    = fmap asin
+  atan    = fmap atan
+  acos    = fmap acos
+  sinh    = fmap sinh
+  tanh    = fmap tanh
+  cosh    = fmap cosh
+  asinh   = fmap asinh
+  atanh   = fmap atanh
+  acosh   = fmap acosh
+
+instance (Bounded b) => Bounded (Wire a b) where
+  minBound = pure minBound
+  maxBound = pure maxBound
 
 stepWire :: Wire a b -> a -> (b, Wire a b)
 stepWire (Wire (Automaton f)) x = let (res, wire') = f x in (res, Wire wire')
