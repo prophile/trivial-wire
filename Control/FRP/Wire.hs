675e5fdacfbf2c4dd1ca6f06f81f261366d35f14
Alastair Lynn <arplynn@gmail.com>
2014-02-18 20:42:55 +0000

    Move convenience instances into a new package
    This means some general cleanup and minuses.

--- Control/FRP/Wire.hs
+++ Control/FRP/Wire.hs
@@ -8,74 +8,33 @@ import Prelude hiding ((.), id)
 import Control.Category
 import Control.Arrow
 import Control.Arrow.Operations(ArrowCircuit)
+import Control.Arrow.Transformer(lift)
 import Control.Arrow.Transformer.Automaton(Automaton(Automaton))
 
-import Control.Applicative(Applicative, pure, liftA2)
-import Data.Monoid(Monoid, mempty, mappend)
-
-import Data.String(IsString, fromString)
+import Control.Arrow.Improve(ImproveArrow, lowerImprove)
 
 -- |Basic FRP wire type. It's actually, in essence, a simple arrow automaton:
 -- isomorphic to Automaton (->) a b. That is, it is roughly correspondant to:
 --
 -- >  type Wire a b = a -> (b, Wire a b)
-newtype Wire a b = Wire (Automaton (->) a b)
+newtype BaseWire a b = BaseWire (Automaton (->) a b)
   deriving (Category, Arrow,
             ArrowChoice,
-            ArrowLoop, ArrowCircuit,
-            Functor, Applicative)
+            ArrowLoop, ArrowCircuit)
 
-instance (Monoid b) => Monoid (Wire a b) where
-  mempty = pure mempty
-  mappend = liftA2 mappend
+type Wire a b = ImproveArrow BaseWire a b
 
-instance (Num b) => Num (Wire a b) where
-  (+) = liftA2 (+)
-  (-) = liftA2 (-)
-  (*) = liftA2 (*)
-
-  negate = fmap negate
-
-  signum = fmap signum
-  abs    = fmap abs
-
-  fromInteger = pure . fromInteger
-
-instance (Fractional b) => Fractional (Wire a b) where
-  (/)          = liftA2 (/)
-  recip        = fmap recip
-  fromRational = pure . fromRational
-
-instance (Floating b) => Floating (Wire a b) where
-  pi      = pure pi
-  exp     = fmap exp
-  sqrt    = fmap sqrt
-  log     = fmap log
-  (**)    = liftA2 (**)
-  logBase = liftA2 logBase
-  sin     = fmap sin
-  tan     = fmap tan
-  cos     = fmap cos
-  asin    = fmap asin
-  atan    = fmap atan
-  acos    = fmap acos
-  sinh    = fmap sinh
-  tanh    = fmap tanh
-  cosh    = fmap cosh
-  asinh   = fmap asinh
-  atanh   = fmap atanh
-  acosh   = fmap acosh
-
-instance (Bounded b) => Bounded (Wire a b) where
-  minBound = pure minBound
-  maxBound = pure maxBound
-
-instance (IsString b) => IsString (Wire a b) where
-  fromString = pure . fromString
+-- |Run a single step of a wire with inputs, getting both the current outputs
+-- and the next iteration of the wire to run.
+stepBaseWire :: BaseWire a b -> a -> (b, BaseWire a b)
+stepBaseWire (BaseWire (Automaton f)) x = (res, BaseWire wire')
+  where (res, wire') = f x
+{-# INLINE stepBaseWire #-}
 
 -- |Run a single step of a wire with inputs, getting both the current outputs
 -- and the next iteration of the wire to run.
 stepWire :: Wire a b -> a -> (b, Wire a b)
-stepWire (Wire (Automaton f)) x = let (res, wire') = f x in (res, Wire wire')
+stepWire w input = (res, lift wire')
+  where (res, wire') = stepBaseWire (lowerImprove w) input
 {-# INLINE stepWire #-}
 
